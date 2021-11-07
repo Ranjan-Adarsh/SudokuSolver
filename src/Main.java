269cb0372059b024dc5124d3cc44b9b6250d659e
adars <adars@LAPTOP-2PDDIU75>
2021-11-07 18:59:53 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Just enter a number below");
+		int a=sc.nextInt();
+		System.out.println("hii there "+a);
+		sc.close();
+	}
+
+}
